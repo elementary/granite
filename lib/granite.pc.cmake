5c2dae5bbb69b427a03841db2645ac3bf3031a63
xapantu <xapantu@gmail.com>
2011-06-13 12:29:35 +0200

    CMake port

---
+++ lib/granite.pc.cmake
@@ -0,0 +1,12 @@
+prefix=@PREFIX@
+exec_prefix=@DOLLAR@{prefix}
+libdir=@DOLLAR@{prefix}/lib
+includedir=@DOLLAR@{prefix}/include
+
+Name: @PKGNAME@
+Description: Granite framework
+Version: 0.1
+Libs: -L@DOLLAR@{libdir} -lgranite
+Cflags: -I@DOLLAR@{includedir}/${PKGNAME}
+Requires: gtk+-3.0
+
