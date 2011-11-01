86073ff5b746956bf25af01f0450489b2b63faf2
xapantu <xapantu@gmail.com>
2011-11-01 18:02:58 +0100

    Disabling gir for now, since we need to have some packages in the ppa

--- cmake/GObjectIntrospectionMacros.cmake
+++ cmake/GObjectIntrospectionMacros.cmake
@@ -1,13 +1,13 @@
 macro(add_target_gir TARGET_NAME GIR_NAME HEADER CFLAGS GRANITE_VERSION)
-    add_custom_target(${TARGET_NAME}-gir ALL DEPENDS ${TARGET_NAME})
-    set(PACKAGES "")
-    foreach(PKG ${ARGN})
-        set(PACKAGES ${PACKAGES} --include=${PKG})
-    endforeach()
-    add_custom_command(TARGET ${TARGET_NAME}-gir COMMAND LD_LIBRARY_PATH="${CMAKE_CURRENT_BINARY_DIR}" g-ir-scanner ${CFLAGS} -n ${GIR_NAME} --quiet --library ${PKGNAME} ${PACKAGES} -o ${GIR_NAME}-${GRANITE_VERSION}.gir --nsversion=${GRANITE_VERSION} ${HEADER}
-        COMMENT "Generating ${GIR_NAME}-${GRANITE_VERSION}.gir")
-    add_custom_command(TARGET ${TARGET_NAME}-gir COMMAND g-ir-compiler ${GIR_NAME}-${GRANITE_VERSION}.gir -o ${GIR_NAME}-${GRANITE_VERSION}.typelib
-        COMMENT "Generating ${GIR_NAME}-${GRANITE_VERSION}.typelib")
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.gir DESTINATION share/gir-1.0/)
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.typelib DESTINATION lib/girepository-1.0/)
+    #    add_custom_target(${TARGET_NAME}-gir ALL DEPENDS ${TARGET_NAME})
+    #set(PACKAGES "")
+    #foreach(PKG ${ARGN})
+    #    set(PACKAGES ${PACKAGES} --include=${PKG})
+    #endforeach()
+    #add_custom_command(TARGET ${TARGET_NAME}-gir COMMAND LD_LIBRARY_PATH="${CMAKE_CURRENT_BINARY_DIR}" g-ir-scanner ${CFLAGS} -n ${GIR_NAME} --quiet --library ${PKGNAME} ${PACKAGES} -o ${GIR_NAME}-${GRANITE_VERSION}.gir --nsversion=${GRANITE_VERSION} ${HEADER}
+    #   COMMENT "Generating ${GIR_NAME}-${GRANITE_VERSION}.gir")
+    #add_custom_command(TARGET ${TARGET_NAME}-gir COMMAND g-ir-compiler ${GIR_NAME}-${GRANITE_VERSION}.gir -o ${GIR_NAME}-${GRANITE_VERSION}.typelib
+    #    COMMENT "Generating ${GIR_NAME}-${GRANITE_VERSION}.typelib")
+    #install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.gir DESTINATION share/gir-1.0/)
+    #install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.typelib DESTINATION lib/girepository-1.0/)
 endmacro()
