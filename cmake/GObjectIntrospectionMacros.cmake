3d6bc1af89469594b2471f06566bd4adb96a06f2
Victor Eduardo <victoreduardm@gmail.com>
2012-11-17 16:55:58 -0600

    CMake Cleanup - Remove option to build static library since it conflicts with the current license (LGPL) - Make the "docs" target build documentation for (both) C and Vala APIs under ${CMAKE_BINARY_DIR}/doc

--- cmake/GObjectIntrospectionMacros.cmake
+++ cmake/GObjectIntrospectionMacros.cmake
@@ -1,16 +1,16 @@
-macro(add_target_gir TARGET_NAME GIR_NAME HEADER C_FILES CFLAGS GRANITE_VERSION)
+macro(add_target_gir TARGET_NAME GIR_NAME HEADER C_FILES CFLAGS PKG_VERSION)
     set(PACKAGES "")
     foreach(PKG ${ARGN})
         set(PACKAGES ${PACKAGES} --include=${PKG})
     endforeach()
     install(CODE "set(ENV{LD_LIBRARY_PATH} \"${CMAKE_CURRENT_BINARY_DIR}:\$ENV{LD_LIBRARY_PATH}\")
     execute_process(COMMAND g-ir-scanner ${CFLAGS} -n ${GIR_NAME}
-            --library ${PKGNAME} ${PACKAGES}
+            --library ${PKG_NAME} ${PACKAGES}
             --warn-all
-            -o ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.gir
+            -o ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${PKG_VERSION}.gir
             -L${CMAKE_CURRENT_BINARY_DIR}
-            --nsversion=${GRANITE_VERSION} ${CMAKE_CURRENT_BINARY_DIR}/${HEADER} ${C_FILES})")
-    install(CODE "execute_process(COMMAND g-ir-compiler ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.gir -o ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.typelib)")
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.gir DESTINATION share/gir-1.0/)
-    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${GRANITE_VERSION}.typelib DESTINATION lib/girepository-1.0/)
+            --nsversion=${PKG_VERSION} ${CMAKE_CURRENT_BINARY_DIR}/${HEADER} ${C_FILES})")
+    install(CODE "execute_process(COMMAND g-ir-compiler ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${PKG_VERSION}.gir -o ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${PKG_VERSION}.typelib)")
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${PKG_VERSION}.gir DESTINATION share/gir-1.0/)
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/${GIR_NAME}-${PKG_VERSION}.typelib DESTINATION lib/girepository-1.0/)
 endmacro()
