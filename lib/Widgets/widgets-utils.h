8418a88e4785f0962b38442c4a45473cbfad7ac7
Lucas Baudin <xapantu@gmail.com>
2014-08-29 12:20:36 -0300

    Solve crash when using the --about command line parameter by avoiding improper type casting.

--- lib/Widgets/widgets-utils.h
+++ lib/Widgets/widgets-utils.h
@@ -1,5 +1,6 @@
 #include <glib.h>
 #include <gtk/gtk.h>
+#include <granite.h>
 
 void    granite_widgets_show_about_dialog (GtkWindow   *parent,
                                            const gchar *first_property_name,
